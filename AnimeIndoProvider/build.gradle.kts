579c242813931048843e90962ed70b9f0e2534bc
szechnaya <redmoeid@gmail.com>
2024-04-09 01:37:07 +0700

    Fix AnimeIndo Provider

--- AnimeIndoProvider/build.gradle.kts
+++ AnimeIndoProvider/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     language = "id"
     // All of these properties are optional, you can safely remove them
 
-    // description = "Lorem Ipsum"
+    description = "Fixed YgY :)"
      authors = listOf("Szechnaya ID")
 
     /**
