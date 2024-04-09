facb1181823bcf9d30d3f8464d7bab3c8b9c4388
szechnaya <134574988+szechnaya@users.noreply.github.com>
2024-04-10 01:52:30 +0700

    Forgot OppaDrama

--- DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
+++ DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
@@ -3,6 +3,6 @@ package com.hexated
 import com.lagradost.cloudstream3.extractors.Filesim
 
 class Oppadrama : DramaidProvider() {
-    override var mainUrl = "https/op3.oppadrama.cc"
+    override var mainUrl = "https://op3.oppadrama.cc"
     override var name = "Oppadrama"
 }
