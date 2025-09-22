ff9994fee78624080c8caa53505054fdcec9c606
suna-aquatope <nanachi.yuzuru@gmail.com>
2025-09-22 22:42:50 +0700

    Update Otakudesu

--- OtakudesuProvider/build.gradle.kts
+++ OtakudesuProvider/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 17
+version = 18
 
 
 cloudstream {
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-    authors = listOf("Hexated")
+    authors = listOf("Szechnaya ID")
 
     /**
      * Status int as the following:
--- OtakudesuProvider/src/main/kotlin/com/hexated/OtakudesuProvider.kt
+++ OtakudesuProvider/src/main/kotlin/com/hexated/OtakudesuProvider.kt
@@ -16,7 +16,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class OtakudesuProvider : MainAPI() {
-    override var mainUrl = "https://otakudesu.cloud"
+    override var mainUrl = "https://otakudesu.best"
     override var name = "Otakudesu"
     override val hasMainPage = true
     override var lang = "id"
