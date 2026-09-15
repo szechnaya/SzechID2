87c931c328fb80961ccb7c991559d61036481e50
szechnaya <134574988+szechnaya@users.noreply.github.com>
2026-09-15 11:12:43 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import com.android.build.gradle.BaseExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io") // Shitpack repo
-}
+        maven("https://jitpack.io")
+    }
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-}
+    }
 }
 
 allprojects {
@@ -21,7 +21,7 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-}
+    }
 }
 
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
@@ -36,10 +36,12 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY")
-?: "https://github.com/hexated/cloudstream-extensions-hexated")
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "https://github.com/hexated/cloudstream-extensions-hexated"
+        )
         authors = listOf("Hexated")
-}
+    }
 
     android {
         namespace = "com.hexated"
@@ -48,24 +50,29 @@ subprojects {
             minSdk = 21
             compileSdkVersion(35)
             targetSdk = 35
-}
+        }
 
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
-}
-}
+        }
+    }
 
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-            freeCompilerArgs += listOf(
+    // Create the custom configuration used by CloudStream.
+    configurations {
+        create("apk")
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+            freeCompilerArgs.addAll(
                 "-Xno-call-assertions",
                 "-Xno-param-assertions",
                 "-Xno-receiver-assertions"
-)
-}
-}
+            )
+        }
+    }
 
     dependencies {
         val apk by configurations
@@ -80,7 +87,7 @@ subprojects {
         implementation("io.karn:khttp-android:0.1.2")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
         implementation("org.mozilla:rhino:1.7.14")
-}
+    }
 }
 
 tasks.register<Delete>("clean") {
